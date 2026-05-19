8682665da21f8a28ffb61c0274a94af45f2fc680
yoel <yoelcruz039@gmail.com>
2026-05-19 10:56:49 -0400

    chore: limpieza de codigo y validacion de cero errores en consola

---
+++ app.js
@@ -0,0 +1,17 @@
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Formulario enviado correctamente");
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Formulario enviado correctamente");
+
+}
